6bf72b861c49b10db71673e34494c17f225b7bf3
Jay Patel <jaypatel87@gmail.com>
2018-12-04 09:39:10 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a generic project template to handle most types of data science projects',
+    author='Jay M. Patel',
+    license='',
+)
